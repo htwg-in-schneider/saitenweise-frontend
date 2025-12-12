82c367d7be347598d5956b5ae7a344950223c40e
Johannes Schneider <johannes.schneider@htwg-konstanz.de>
2025-12-12 14:21:07 +0100

    adding auth0 config to production

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const auth0 = createAuth0({
     clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
     authorizationParams: {
         audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin + window.location.pathname
     }
 })
 
