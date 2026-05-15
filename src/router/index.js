efa117154032857cd3154d1b614437e6efac72d0
Johannes Schneider <johannes.schneider@htwg-konstanz.de>
2026-05-15 11:55:39 +0200

    Iteration 12a: User Authentication with Auth0

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,18 @@ import ProductCatalog from '@/views/ProductCatalog.vue';
 import ProductDetail from '@/views/ProductDetail.vue';
 import CreateProduct from '@/views/CreateProduct.vue';
 import EditProduct from '@/views/EditProduct.vue';
+import Profile from '@/views/Profile.vue';
 
 const routes = [
   { path: '/', component: ProductCatalog },
-  { 
-    path: '/product/view/:id', 
+  {
+    path: '/product/view/:id',
     name: 'product',
     component: ProductDetail
   },
   { path: '/product/create', component: CreateProduct },
   { path: '/product/edit/:id', component: EditProduct },
+  { path: '/profile', component: Profile },
 ];
 
 const router = createRouter({
