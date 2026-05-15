3368e5696026b51a2dea6f26cbc7f59ac8abd223
Johannes Schneider <johannes.schneider@htwg-konstanz.de>
2026-05-15 11:55:39 +0200

    Iteration 12a: User Authentication with Auth0

--- src/main.js
+++ src/main.js
@@ -4,10 +4,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import { createAuth0 } from '@auth0/auth0-vue'
 
 const pinia = createPinia()
+
+const auth0 = createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        redirect_uri: window.location.origin + window.location.pathname
+    }
+})
 
 createApp(App)
     .use(router)
     .use(pinia)
+    .use(auth0)
     .mount('#app')
